007b31ea61303cb9a09f6d0b02127fdff6f9fac4
NahBertordo <40553388+NahBertordo@users.noreply.github.com>
2023-11-22 11:46:55 -0300

    Add files via upload

---
+++ pages/forgot.js
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import { setCookie } from 'cookies-next'
+import { useRouter } from 'next/router'
+
+import Link from 'next/link'
+
+import styles from '../styles/Login.module.css'
+
+import LoginCard from '../components/cards/loginCard/login'
+import Input from '../components/forms/input/input'
+import Button from '../components/forms/button/button'
+
+export default function ForgotPage() {
+  const [form, setForm] = useState({
+    email: ''
+  })
+  const [error, setError] = useState('')
+  const router = useRouter()
+
+  const handleChangeForm = (event, field) => {
+    setForm({
+      ...form,
+      [field]: event.target.value
+    })
+  }
+
+  const handleForm = async (e) => {
+    e.preventDefault()
+
+    if (!form.email) return setError('O e-mail é obrigatório')
+
+    setError('')
+    try {
+      const response = await fetch('/api/user/forgot', {
+        method: 'POST',
+        body: JSON.stringify(form)
+      })
+
+      const json = await response.json()
+
+      if (response.status !== 200) throw new Error(json)
+      setCookie('authorization', json)
+      router.push('/')
+    } catch (err) {
+      setError(err.message)
+    }
+  }
+
+  return (
+    <div className={styles.background}>
+      <LoginCard title="Recupere sua senha">
+        <form className={styles.form}>
+          <Input type="e-mail" placeholder="Seu e-mail" value={form['email']} onChange={(event) => handleChangeForm(event, 'email')} />
+          <Button onClick={handleForm}>Continuar</Button>
+          {error && <p className="error">{error}</p>}
+        </form>
+      </LoginCard>
+    </div>
+  )
+}
